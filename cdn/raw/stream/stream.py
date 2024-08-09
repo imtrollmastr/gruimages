88febfb80bed0f2cd340c644beaad396abfaf411
eddie stockton <eddiestockton420@gmail.com>
2024-08-09 22:34:45 +0800

    stream

--- cdn/raw/stream/stream.py
+++ cdn/raw/stream/stream.py
@@ -1,13 +1,18 @@
 import requests
+import base64
+import time
 
-def updateFile(y):
+def updateFile(y1, y2, y3, y4, y5):
     with open("stream/index.html", "wb") as x:
-        q = "<html><head><title>GRU Streaming Element</title></head><body><p>" + y + " has joined as a GRU member.</p></body></html>"
+        q = "<html><head><title>GRU Streaming Element</title></head><body><div><p>" + y1 + " has joined as a GRU member.</p></div><div><p>" + y2 + " has joined as a GRU member.</p></div><div><p>" + y3 + " has joined as a GRU member.</p></div><div><p>" + y4 + " has joined as a GRU member.</p></div><div><p>" + y5 + " has joined as a GRU member.</p></div></body></html>"
         q = q.encode("utf-8")
         x.write(bytes(q))
-
-r = requests.get('https://api.ipdata.co?api-key=b6c69cd24db05d3d56e2cb4e47a51bae7775bf82f63e70ec76ff23f6').json()
-ip_address = r["ip"]
         
 while True:
-    updateFile(ip_address)
+    r1 = str(base64.b64decode(bytes("KHVzZXJuYW1lKQ==".encode("utf-8"))))
+    r2 = str(base64.b64decode(bytes("KHVzZXJuYW1lKQ==".encode("utf-8"))))
+    r3 = str(base64.b64decode(bytes("KHVzZXJuYW1lKQ==".encode("utf-8"))))
+    r4 = str(base64.b64decode(bytes("KHVzZXJuYW1lKQ==".encode("utf-8"))))
+    r5 = str(base64.b64decode(bytes("KHVzZXJuYW1lKQ==".encode("utf-8"))))
+    updateFile(r1, r2, r3, r4, r5)
+    time.sleep(60)
